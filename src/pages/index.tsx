9a80469a26c7ad882dbbfd24635b2d2729d64091
Erika Perciliano <erikaperciliano.developer@gmail.com>
2024-07-17 11:11:04 +0100

    add style configuration through the server using stitches

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,11 @@
+import { styled } from "./styles"
+
+const Button = styled('button', {
+    backgroundColor: '$rocketseat'
+})
+
 export default function Home() {
     return (
-        <h1>SHOP</h1>
+       <Button>Send</Button>
     )
 }
