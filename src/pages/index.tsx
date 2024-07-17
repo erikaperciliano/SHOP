01c82ba8bdd041a61dba67c39214ceb460c54e81
Erika Perciliano <erikaperciliano.developer@gmail.com>
2024-07-17 11:22:09 +0100

    adds global styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,25 @@
 import { styled } from "./styles"
 
 const Button = styled('button', {
-    backgroundColor: '$rocketseat'
+    backgroundColor: '$green500',
+    borderRadius: 4,
+    border: 0,
+    padding: '4px 8px',
+
+    span: {
+        fontWeight: 'bold'
+    },
+
+    '&:hover': {
+        filter: 'brightness(0.8)'
+    }
 })
 
 export default function Home() {
     return (
-       <Button>Send</Button>
+       <Button>
+        <span>Testttttttt</span>
+        Send
+        </Button>
     )
 }
