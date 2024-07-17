1c2e61d874cd47326011d43be19eef0fe3394e52
Erika Perciliano <erikaperciliano.developer@gmail.com>
2024-07-17 17:50:43 +0100

    adds product page structure

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,25 @@
 import { useRouter } from "next/router"
+import { ImageContainer, ProductContainer, ProductDetails } from "./product"
 
 export default function Product(){
     const { query } = useRouter()
 
     return(
-        <h1>Product: {JSON.stringify(query)}</h1>
+       <ProductContainer>
+        <ImageContainer>
+            
+        </ImageContainer>
+
+        <ProductDetails>
+            <h1>Shirt X</h1>
+            <span> £ 29.90</span>
+
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto sint perspiciatis, rerum debitis non earum enim repudiandae eos in quasi beatae necessitatibus dolorem suscipit? Illum laudantium id quisquam facilis provident?
+            </p>
+
+            <button>Buy Now</button>
+        </ProductDetails>
+       </ProductContainer>
     )
 }
