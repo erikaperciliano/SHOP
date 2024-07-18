4dab298f0676ec9fc6ff540c06f213ae838748c8
Erika Perciliano <erikaperciliano.developer@gmail.com>
2024-07-18 10:58:19 +0100

    using Site Static Generation (SSG)

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,25 +1,98 @@
-import { useRouter } from "next/router"
-import { ImageContainer, ProductContainer, ProductDetails } from "./product"
+import { ImageContainer, ProductContainer, ProductDetails } from "@/styles/pages/product"
 
-export default function Product(){
-    const { query } = useRouter()
+import { GetStaticPaths, GetStaticProps } from "next"
+
+import { stripe } from "@/lib/stripe"
+import Stripe from "stripe"
+import Image from "next/image"
+import axios from "axios"
+import { useState } from "react"
+
+interface ProductProps {
+    product: {
+        id: string;
+        name: string;
+        imageUrl: string;
+        price: string;
+        description: string;
+        defaultPriceId: string;
+    }
+}
+
+export default function Product({ product }: ProductProps){
+    const [isCreatingCheckoutSession, setIsCreatingCheckoutSession] = useState(false)
+
+    async function handleBuyProduct(){
+        setIsCreatingCheckoutSession(true)
+
+        try{
+            const response = await axios.post('/api/checkout', {
+                priceId: product.defaultPriceId
+            })
+
+            const { checkoutUrl } = response.data;
+            
+            window.location.href = checkoutUrl
+        }catch(err){
+            setIsCreatingCheckoutSession(false)
+            alert('Failed to redirect checkout')
+        }
+    }
 
     return(
        <ProductContainer>
         <ImageContainer>
-            
+            <Image src={product.imageUrl} width={520} height={480} alt="product image"/>
         </ImageContainer>
 
         <ProductDetails>
-            <h1>Shirt X</h1>
-            <span> £ 29.90</span>
+            <h1>{product.name}</h1>
+            <span>{product.price}</span>
 
             <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto sint perspiciatis, rerum debitis non earum enim repudiandae eos in quasi beatae necessitatibus dolorem suscipit? Illum laudantium id quisquam facilis provident?
+                {product.description}
             </p>
 
-            <button>Buy Now</button>
+            <button disabled={isCreatingCheckoutSession} onClick={handleBuyProduct}>Buy Now</button>
         </ProductDetails>
        </ProductContainer>
     )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+    // Search for best-selling products
+
+    return {
+        paths: [
+            { params: { id: 'prod_QUXg4cfQ2G7NLc' }}
+        ],
+        fallback: 'blocking'
+    }
+}
+
+export const getStaticProps: GetStaticProps<any, { id: string }> = async ({ params }) => {
+    const productId = params?.id
+
+    const product = await stripe.products.retrieve(productId!, {
+        expand: ['default_price']
+    })
+
+    const price = product.default_price as Stripe.Price
+
+    return {
+        props: {
+            product: {
+                id: product.id,
+                name: product.name,
+                imageUrl: product.images[0],
+                price: new Intl.NumberFormat('de-DE', {
+                    style: 'currency',
+                    currency: 'EUR'
+                }).format(price.unit_amount! / 100),
+                description: product.description,
+                defaultPriceId: price.id
+            }
+        },
+        revalidate: 60 * 60 * 1 // 1hour
+    }
 }
